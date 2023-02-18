9722a7c4420a37ec7ed802d5ab8e811107376271
alexanderboric <alexanderboric04@gmail.com>
2023-02-18 19:54:14 +0100

    changed initialization

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -7,7 +7,7 @@ int stepsPerRevolution = 800;  // change this to fit the number of steps per rev
 
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(stepsPerRevolution, 2,3,4,5);
-AccelStepper myAccStepper= new AccelStepper( 2,3,4,5,true);
+AccelStepper myAccStepper( 2,3,4,5,true);
 myAccStepper.setMaxSpeed(80.0);
 myAccStepper.setSpeed(70.0);
 
